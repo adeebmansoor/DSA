d3074db0707ec397d318b61f02466f8bce629712
adeeb mansoor <adeebmansoor17@gmail.com>
2025-03-02 03:20:04 +0530

    new

--- arrayList.java
+++ arrayList.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class arrayList {
+public class ArrayList {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         // ArrayList<Integer> list = new ArrayList<>(10);
