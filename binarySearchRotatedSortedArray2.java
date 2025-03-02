14c824d3af2054e9c39fd0cdda7d1e940428795c
adeeb mansoor <adeebmansoor17@gmail.com>
2025-03-03 01:26:34 +0530

    new

--- binarySearchRotatedSortedArray2.java
+++ binarySearchRotatedSortedArray2.java
@@ -1,5 +1,6 @@
-public class binarySearchRotatedSortedArray {
-    static int pivot(int[] arr){
+// Rotated sorted array with duplicate values
+public class binarySearchRotatedSortedArray2 {
+    static int pivotWithDuplicates(int[] arr){
         int start = 0;
         int end = arr.length-1;
         while(start<=end){
@@ -16,22 +17,35 @@ public class binarySearchRotatedSortedArray {
             else if((mid > start) && (arr[mid] < arr[mid - 1])){
                 return mid - 1;
             }
-            // case 3: if start element is greater than equal to the mid element it means the pivot will lie on the left side of the array
-            // so we will reduce the search space by making end = mid-1
-            else if(arr[0] >= arr[mid]){
-                end = mid - 1;
+            // case 3: this is to check if the start, end & mid values are all equal or duplicates
+            // if its true the we will eliminate the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]){
+                // checking if the start value we are skipping is not the pivot
+                // if it is then we will return it, else skip it
+                if(arr[start] > arr[start + 1]){
+                    return start;
+                }
+                start++;
+                // checking if the end value we are skipping is not the pivot
+                // if it is then we will return it, else skip it
+                if(arr[end] < arr[end - 1]){
+                    return end - 1;
+                }
+                end --;
             }
-            // case 4: if start element is less than the mid element it means the pivot will lie on the right side of the array
-            // so we will reduce the search space by making start = mid+1
-            else{
+            // left side is sorted, search pivot in the right side
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])){
                 start = mid + 1;
+            }
+            else{
+                end = mid - 1;
             }
         }
         // otherwise return -1
         return -1;
     }
     static int search(int[] arr, int target){
-        int pivot = pivot(arr);
+        int pivot = pivotWithDuplicates(arr);
         // case 1: if you do not find the pivot that means the array is not rotated
         // just do normal binary search for this case
         if (pivot == -1){
@@ -67,8 +81,8 @@ public class binarySearchRotatedSortedArray {
         return -1;
     }
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,8,0,1,2,3};
-        int target = 6;
+        int[] arr = {2,8,9,2,2,2,6,2};
+        int target = 8;
         System.out.println(search(arr,target));
     }
 }
---
+++ binarySearchRotationCount.java
@@ -0,0 +1,26 @@
+public class binarySearchRotationCount {
+    static int pivot(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end) {
+            int mid = start + (end - start)/2;
+            if (mid < end && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            else if(mid > start && arr[mid] < arr[mid-1]){
+                return mid - 1;
+            }
+            else if(arr[start] > arr[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(pivot(arr) + 1);
+    }
+}
